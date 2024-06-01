6293760f70a1744e41b8c08f7982f2a297ce118a
PrinceCatt <2991588053@qq.com>
2024-06-01 16:29:55 +0800

    push 7, by yitao, profile page

--- frontend/src/view/my/profile/index.vue
+++ frontend/src/view/my/profile/index.vue
@@ -1,18 +1,22 @@
 <template>
     <div>
-        <h1>Email:</h1>
-        <h3>{{email}}</h3>
-        <h1>Username:</h1>
-        <el-button type="text" @click="changeName">changeName</el-button>
-        <h1>Avatar:</h1>
-        <div class="demo-image">
-  <div class="block" v-for="fit in fits" :key="fit">
+
+
+
+  <el-col :span="6"><div class="grid-content bg-purple"></div><h1>Email:</h1>
+        <h3>{{email}}</h3></el-col>
+
+  <el-col :span="6"><div class="grid-content bg-purple"></div><h1>Username:</h1>
+        <el-button type="text" @click="changeName">changeName</el-button></el-col>
+  <el-col :span="6"><div class="grid-content bg-purple"></div>  <div class="block" v-for="fit in fits" :key="fit">
+    <h1>Avatar:</h1>
     <el-image
       style="width: 300px; height: 300px"
-      :src= avatar
+      :src= "avatar"
       :fit="fit"></el-image>
-      <h3>Change Avatar</h3>
-      <el-upload
+      
+  </div></el-col>
+  <el-col :span="6"><div class="grid-content bg-purple"></div><h3>Change Avatar</h3><el-upload
   class="avatar-uploader"
   action="https://jsonplaceholder.typicode.com/posts/"
   :show-file-list="false"
@@ -20,9 +24,8 @@
   :before-upload="beforeAvatarUpload">
   <img v-if="imageUrl" :src="imageUrl" class="avatar">
   <i v-else class="el-icon-plus avatar-uploader-icon"></i>
-</el-upload>
-  </div>
-</div>
+</el-upload></el-col>
+
     </div>
 </template>
 
@@ -99,5 +102,33 @@
     width: 178px;
     height: 178px;
     display: block;
+  }
+
+
+  .el-row {
+    margin-bottom: 20px;
+    &:last-child {
+      margin-bottom: 0;
+    }
+  }
+  .el-col {
+    border-radius: 4px;
+  }
+  .bg-purple-dark {
+    background: #99a9bf;
+  }
+  .bg-purple {
+    background: #d3dce6;
+  }
+  .bg-purple-light {
+    background: #e5e9f2;
+  }
+  .grid-content {
+    border-radius: 4px;
+    min-height: 36px;
+  }
+  .row-bg {
+    padding: 10px 0;
+    background-color: #f9fafc;
   }
 </style>
