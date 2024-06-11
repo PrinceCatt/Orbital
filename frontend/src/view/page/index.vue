ccaa6b111c314aba47d88931314e6154d4eaba7b
PrinceCatt <2991588053@qq.com>
2024-06-11 20:49:08 +0800

    push 11 by yitao, posts request and render

--- frontend/src/view/page/index.vue
+++ frontend/src/view/page/index.vue
@@ -6,31 +6,81 @@
 
         <el-button @click="open($route.params.id)"> Open </el-button>
         <el-button @click="newPost()"> New Post </el-button>
+
+<el-row>
+  <el-col :span="24"><div class="grid-content bg-purple-dark"><el-button @click="open($route.params.id, 1)"> Post 1 </el-button></div></el-col>
+</el-row>
+<el-row>
+  <el-col :span="24"><div class="grid-content bg-purple-dark">
+    
+    <ul>
+      <li v-for="post in posts" :key="post.id">
+        <h3>{{post.title}}</h3>
+      </li>
+    </ul>
+
+  </div></el-col>
+</el-row>
+
+
     </div>
 </template>
 
 
+<style>
+  .el-row {
+    margin-bottom: 20px;
+    &:last-child {
+      margin-bottom: 0;
+    }
+  }
+  .el-col {
+    border-radius: 4px;
+  }
+  .bg-purple-dark {
+    background: #99a9bf;
+  }
+  .bg-purple {
+    background: #d3dce6;
+  }
+  .bg-purple-light {
+    background: #e5e9f2;
+  }
+  .grid-content {
+    border-radius: 4px;
+    min-height: 36px;
+  }
+  .row-bg {
+    padding: 10px 0;
+    background-color: #f9fafc;
+  }
+</style>
+
 <script>
 
-
+import {newPost, getPost} from '@/api/post'
 
 export default {
+  props:[posts,pageId],
 
+    created(){
+      Promise((resolve, reject) => {getPost(this.$route.params.id, pageId).then(response => {
+                posts = response
+                resolve()
+            }).catch(error => {
+                reject(error)
+                return('Error in loading posts')
+          })
+
+      })
+    },
     methods: {
-        open(id) {
-            if(id == 1){
-            alert("1")
-            }
-            else {
-                alert("not 1")
-            }
+        open(sectionId, postId) {          
 
-            this.$store.dispatch(getPost(id))
-            
         },
 
         newPost(){
-            this.$router.push({path: '/login'})
+            this.$router.push({path: '/newpost'})
         }
       }
 }
