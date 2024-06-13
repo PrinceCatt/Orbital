bd79f9cf77694faee0cb5076aa877db8d9c6b9b7
PrinceCatt <2991588053@qq.com>
2024-06-13 20:51:45 +0800

    aaa

--- frontend/src/view/page/index.vue
+++ frontend/src/view/page/index.vue
@@ -1,85 +1,78 @@
 <template>
-    
-    <div>
-        <h3>{{ $route.params.id }}</h3>
-        <el-button @click="newPost()"> New Post </el-button>
+  <div>
+    <h3>{{ $route.params.id }}</h3>
+    <el-button @click="newPost()"> New Post </el-button>
 
-
-<el-row>
-  <el-col :span="24"><div class="grid-content bg-purple-dark">
-    
-    <ul>
-      <li v-for="post in posts" :key="post.id">
-        <h3>{{post.title}}</h3>
-      </li>
-    </ul>
-    <el-pagination
-  background
-  layout="prev, pager, next"
-  :total="100">
-</el-pagination>
-  </div></el-col>
-</el-row>
-
-
-    </div>
+    <el-row>
+      <el-col :span="24"
+        ><div class="grid-content bg-purple-dark">
+          <ul>
+            <li v-for="post in posts" :key="post.id">
+              <h3>{{ post.title }}</h3>
+            </li>
+          </ul>
+          <el-pagination background layout="prev, pager, next" :total="100">
+          </el-pagination></div
+      ></el-col>
+    </el-row>
+  </div>
 </template>
 
 <script>
-
-import {getPost} from '@/api/post'
+import { getPost } from "@/api/post";
 
 export default {
-  props:['posts','pageId'],
+  props: ["posts","pageId"],
 
-    created(){
-      Promise((resolve, reject) => {getPost(this.$route.params.id, pageId).then(response => {
-                posts = response
-                resolve()
-            }).catch(error => {
-                reject(error)
-                return('Error in loading posts')
-          })
-      })
+  mounted() {
+    new Promise((resolve, reject) => {
+      getPost(this.$route.params.id, pageId)
+        .then((response) => {
+          posts = response;
+          resolve();
+        })
+        .catch((error) => {
+          reject(error);
+          return "Error in loading posts";
+        });
+    });
+  },
+  methods: {
+    open(sectionId, postId) {},
+
+    newPost() {
+      this.$router.push({ path: "/newpost" });
     },
-    methods: {
-        open(sectionId, postId) {          
-
-        },
-
-        newPost(){
-          this.$router.push({path: '/newpost'})
-        }
-      }
-}
+  },
+};
 </script>
 
-<style scoped>
-  .el-row {
-    margin-bottom: 20px;
-    &:last-child {
-      margin-bottom: 0;
-    }
+<style lang="less" scoped>
+.el-row {
+  margin-bottom: 20px;
+  &:last-child {
+    margin-bottom: 0;
   }
-  .el-col {
-    border-radius: 4px;
-  }
-  .bg-purple-dark {
-    background: #99a9bf;
-  }
-  .bg-purple {
-    background: #d3dce6;
-  }
-  .bg-purple-light {
-    background: #e5e9f2;
-  }
-  .grid-content {
-    border-radius: 4px;
-    min-height: 36px;
-  }
-  .row-bg {
-    padding: 10px 0;
-    background-color: #f9fafc;
-  }
+}
+.el-col {
+  border-radius: 4px;
+}
+.bg-purple-dark {
+  background: #99a9bf;
+}
+.bg-purple {
+  background: #d3dce6;
+}
+.bg-purple-light {
+  background: #e5e9f2;
+}
+.grid-content {
+  border-radius: 4px;
+  min-height: 36px;
+}
+.row-bg {
+  padding: 10px 0;
+  background-color: #f9fafc;
+}
 </style>
 
