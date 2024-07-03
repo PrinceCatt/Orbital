354c1d691370f2d9cecd2deb19d97a7094843416
PrinceCatt <2991588053@qq.com>
2024-07-03 22:26:25 +0800

    debug2

--- backend/backend/src/main/java/org/example/backend/utils/RedisKeyUtils.java
+++ backend/backend/src/main/java/org/example/backend/utils/RedisKeyUtils.java
@@ -2,9 +2,11 @@ package org.example.backend.utils;
 
 public class RedisKeyUtils {
 
+    //the key for two hashmaps for liked and likedCount
     public static final String MapUserLiked = "MapUserLiked";
     public static final String MapUserLikesCount = "MapUserLikesCount";
 
+    //generate key for storing in hashmap
     public static String getLikedKey(int commentId, int giveUserId) {
         StringBuilder builder = new StringBuilder();
         builder.append(commentId);
